6394d80574c8403726737a30fd4365c08e5c6abf
Robert Schwarz <mail@rschwarz.net>
2012-11-02 08:20:42 +0100

    customized pytyle keybindings

--- pytyle3/keybind.py
+++ pytyle3/keybind.py
@@ -2,21 +2,16 @@ import state
 import tile
 
 bindings = {
-    'Mod1-a':       tile.cmd('tile'),
-    'Mod1-u':       tile.cmd('untile'),
-    'Mod1-h':       tile.cmd('decrease_master'),
-    'Mod1-l':       tile.cmd('increase_master'),
-    'Mod1-j':       tile.cmd('prev_client'),
-    'Mod1-k':       tile.cmd('next_client'),
-    'Mod1-Shift-j': tile.cmd('switch_prev_client'),
-    'Mod1-Shift-k': tile.cmd('switch_next_client'),
-    'Mod1-comma':   tile.cmd('remove_master'),
-    'Mod1-period':  tile.cmd('add_master'),
-    'Mod1-Return':  tile.cmd('make_master'),
-    'Mod1-m':       tile.cmd('focus_master'),
-    'Mod1-z':       tile.cmd('cycle'),
-
-    'Mod1-q':       tile.debug_state,
-    'Mod1-Shift-q': state.quit,
+    'Mod4-q': tile.cmd('decrease_master'),
+    'Mod4-w': tile.cmd('increase_master'),
+    'Mod4-e': tile.cmd('tile'),
+    'Mod4-r': tile.cmd('untile'),
+    'Mod4-a': tile.cmd('remove_master'),
+    'Mod4-s': tile.cmd('add_master'),
+    'Mod4-z': tile.cmd('make_master'),
+    'Mod4-x': tile.cmd('focus_master'),
+    'Mod4-c': tile.cmd('cycle'),
 }
 
+# 'Mod4-j':       tile.cmd('prev_client'),
+# 'Mod4-k':       tile.cmd('next_client'),
