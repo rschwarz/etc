071ef64763362f6a15d90e2abfe6c395da1448f5
Robert Schwarz <mail@rschwarz.net>
2012-11-04 13:25:16 +0100

    switch pytyle keybindings

--- pytyle3/keybind.py
+++ pytyle3/keybind.py
@@ -6,12 +6,9 @@ bindings = {
     'Mod4-w': tile.cmd('increase_master'),
     'Mod4-e': tile.cmd('tile'),
     'Mod4-r': tile.cmd('untile'),
-    'Mod4-a': tile.cmd('remove_master'),
-    'Mod4-s': tile.cmd('add_master'),
+    'Mod4-a': tile.cmd('add_master'),
+    'Mod4-s': tile.cmd('remove_master'),
     'Mod4-z': tile.cmd('make_master'),
     'Mod4-x': tile.cmd('focus_master'),
     'Mod4-c': tile.cmd('cycle'),
 }
-
-# 'Mod4-j':       tile.cmd('prev_client'),
-# 'Mod4-k':       tile.cmd('next_client'),
