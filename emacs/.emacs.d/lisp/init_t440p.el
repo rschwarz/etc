e132d13dc9a410e55a3a66f7b4110d568b490c4c
Robert Schwarz <mail@rschwarz.net>
2017-03-01 22:19:04 +0100

    emacs: choose browser for t440p

---
+++ emacs/.emacs.d/lisp/init_t440p.el
@@ -0,0 +1,5 @@
+;; customizations to be used only on this machine
+
+;; open links from org in chromium, not firefox
+(setq browse-url-browser-function 'browse-url-generic
+      browse-url-generic-program "chromium")
