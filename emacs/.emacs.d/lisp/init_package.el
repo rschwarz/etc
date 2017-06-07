73df79f0b67b6459975cc49a4e77155f57912c0a
Robert Schwarz <mail@rschwarz.net>
2017-06-07 10:33:36 +0700

    emacs: rm marmalade repo

--- emacs/.emacs.d/lisp/init_package.el
+++ emacs/.emacs.d/lisp/init_package.el
@@ -5,7 +5,6 @@
 
 ;; add repos
 (package-initialize)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
