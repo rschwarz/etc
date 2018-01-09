1800a79e947fafd9681ff3c23ac4a5b8dc60bca5
Robert Schwarz <mail@rschwarz.net>
2018-01-09 14:03:02 +0100

    emacs: enable fuzzy matching in helm

--- emacs/.emacs.d/lisp/init_helm.el
+++ emacs/.emacs.d/lisp/init_helm.el
@@ -4,3 +4,9 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-x b") 'helm-buffers-list)
+
+; enable more fuzzy matching
+;; (setq helm-buffers-fuzzy-matching t)
+;; (setq helm-M-x-fuzzy-match t)
+(setq helm-mode-fuzzy-match t)
+(setq helm-completion-in-region-fuzzy-match t)
