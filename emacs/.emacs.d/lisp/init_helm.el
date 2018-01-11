7f1bb8424ffbaedc8e128c9664c923fd52bbff01
Robert Schwarz <mail@rschwarz.net>
2018-01-11 14:34:54 +0100

    emacs: re-enable helm fuzzy matching (setting was wrong?)

--- emacs/.emacs.d/lisp/init_helm.el
+++ emacs/.emacs.d/lisp/init_helm.el
@@ -6,7 +6,7 @@
 (global-set-key (kbd "C-x b") 'helm-buffers-list)
 
 ; enable more fuzzy matching
-;; (setq helm-buffers-fuzzy-matching t)
-;; (setq helm-M-x-fuzzy-match t)
+(setq helm-buffers-fuzzy-matching t)
+(setq helm-M-x-fuzzy-match t)
 (setq helm-mode-fuzzy-match t)
 (setq helm-completion-in-region-fuzzy-match t)
