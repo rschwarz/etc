baada13cc25ec6348075df60f8d45e10f8bcb206
Robert Schwarz <mail@rschwarz.net>
2018-02-14 10:15:17 +0100

    emacs: more docker packages

--- emacs/.emacs.d/lisp/init_docker.el
+++ emacs/.emacs.d/lisp/init_docker.el
@@ -3,3 +3,9 @@
 ;; see https://github.com/spotify/dockerfile-mode
 (use-package dockerfile-mode)
 (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+
+;; editing and completion for docker compose (YAML)
+(use-package docker-compose-mode)
+
+;; managing docker images and containers
+(use-package docker)
