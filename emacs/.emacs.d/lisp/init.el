097b4d37c34f03afb26d83169a2cf9d6d2f2c514
Robert Schwarz <mail@rschwarz.net>
2018-02-20 17:21:12 +0100

    emacs: use my own sql-clickhouse definitions

--- emacs/.emacs.d/lisp/init.el
+++ emacs/.emacs.d/lisp/init.el
@@ -34,6 +34,7 @@
 (load "init_org.el")
 (load "init_python.el")
 ;; (load "init_smex.el")
+(load "init_sql.el")
 (load "init_tex.el")
 (load "init_web.el")
 (load "init_writegood.el")
---
+++ emacs/.emacs.d/lisp/init_sql.el
@@ -0,0 +1,4 @@
+;; SQL specific customizations
+
+(add-to-list 'load-path "~/src/sql-clickhouse/")
+(use-package sql-clickhouse :ensure nil)
