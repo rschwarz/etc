5766eed5ba6a125881e7f39c49bf6611aae6a098
Robert Schwarz <mail@rschwarz.net>
2018-11-16 12:46:29 +0100

    emacs: use indent-tools with YAML

--- emacs/.emacs.d/lisp/init_yaml.el
+++ emacs/.emacs.d/lisp/init_yaml.el
@@ -3,3 +3,6 @@
 (use-package yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-mode))
+
+(use-package indent-tools)
+(add-hook 'yaml-mode-hook (lambda () (indent-tools-minor-mode 1)))
