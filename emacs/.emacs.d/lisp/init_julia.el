87a4c4086933172865c41bb398e4cf2e260d65de
Robert Schwarz <mail@rschwarz.net>
2019-03-10 20:45:38 +0100

    emacs: use julia-repl instead of julia-shell

--- emacs/.emacs.d/lisp/init_julia.el
+++ emacs/.emacs.d/lisp/init_julia.el
@@ -1,17 +1,11 @@
 ;; julia specific customizations
+
 (use-package julia-mode)
-(use-package julia-shell)
+(use-package julia-repl)
 
-(setq julia-shell-program "~/opt/julia/bin/julia")
-(setq julia-shell-animate-logo nil)
-
-(defun rs-julia-mode-config ()
-  (local-set-key (kbd "C-c C-c") 'julia-shell-run-region-or-line)
-  )
-(add-hook 'julia-mode-hook 'rs-julia-mode-config)
-
-(add-hook 'julia-mode-hook   (lambda () (subword-mode 1)))
-(add-hook 'julia-mode-hook   (lambda () (rainbow-delimiters-mode 1)))
-(add-hook 'julia-mode-hook   (lambda () (fci-mode 1)))
-(add-hook 'julia-mode-hook   (lambda () (idle-highlight-mode 1)))
+(add-hook 'julia-mode-hook 'julia-repl-mode)
+(add-hook 'julia-mode-hook (lambda () (subword-mode 1)))
+(add-hook 'julia-mode-hook (lambda () (rainbow-delimiters-mode 1)))
+(add-hook 'julia-mode-hook (lambda () (fci-mode 1)))
+(add-hook 'julia-mode-hook (lambda () (idle-highlight-mode 1)))
 (add-hook 'julia-mode-hook 'ws-butler-mode)
