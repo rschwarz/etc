d43c3af79678d5bcff0217cff88aea69610d3e43
Robert Schwarz <mail@rschwarz.net>
2019-03-18 14:17:40 +0100

    emacs: write email with org-mode

--- emacs/.emacs.d/lisp/init_mail.el
+++ emacs/.emacs.d/lisp/init_mail.el
@@ -1,5 +1,4 @@
 ;; customize modes for editing messages
 
-(add-to-list 'auto-mode-alist '("tmpmsg\\." . message-mode))
-(add-to-list 'auto-mode-alist '("\\.eml\\'" . message-mode))
-(add-hook 'message-mode-hook 'turn-on-orgstruct)
+(add-to-list 'auto-mode-alist '("tmpmsg\\." . org-mode))
+(add-to-list 'auto-mode-alist '("\\.eml\\'" . org-mode))
