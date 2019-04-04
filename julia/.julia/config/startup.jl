7f2a4b508fe8e3c47397306a4c80912cfc2b7ce8
Robert Schwarz <mail@rschwarz.net>
2019-04-04 21:07:09 +0200

    julia: enable Revise

--- julia/.julia/config/startup.jl
+++ julia/.julia/config/startup.jl
@@ -5,3 +5,12 @@ function activenv(name::String)
     root = joinpath(ENV["HOME"], ".julia", "environments")
     Pkg.activate(joinpath(root, name))
 end
+
+# auto-enable Revise
+atreplinit() do repl
+    try
+        @eval using Revise
+        @async Revise.wait_steal_repl_backend()
+    catch
+    end
+end
