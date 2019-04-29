755f0f3908f033c346270179aad3fcdcf9d0953d
Robert Schwarz <mail@rschwarz.net>
2019-04-29 09:29:00 +0200

    emacs: use simple mail-mode for mails (to reflow comments)

--- emacs/.emacs.d/lisp/init_mail.el
+++ emacs/.emacs.d/lisp/init_mail.el
@@ -1,4 +1,4 @@
 ;; customize modes for editing messages
 
-(add-to-list 'auto-mode-alist '("tmpmsg\\." . org-mode))
-(add-to-list 'auto-mode-alist '("\\.eml\\'" . org-mode))
+(add-to-list 'auto-mode-alist '("tmpmsg\\." . mail-mode))
+(add-to-list 'auto-mode-alist '("\\.eml\\'" . mail-mode))
