3ca853adc742bdfe00f63c7dc1c5d979b8417e91
Robert Schwarz <mail@rschwarz.net>
2020-05-04 13:29:02 +0200

    emacs: use julia-snail, no longer julia-repl

--- emacs/.emacs.d/lisp/init_julia.el
+++ emacs/.emacs.d/lisp/init_julia.el
@@ -1,13 +1,13 @@
 ;; julia specific customizations
 
 (use-package julia-mode)
-(use-package julia-repl)
 
-(add-hook 'julia-mode-hook 'julia-repl-mode)
 (add-hook 'julia-mode-hook (lambda () (subword-mode 1)))
 (add-hook 'julia-mode-hook (lambda () (rainbow-delimiters-mode 1)))
 (add-hook 'julia-mode-hook (lambda () (fci-mode 1)))
 (add-hook 'julia-mode-hook (lambda () (idle-highlight-mode 1)))
 (add-hook 'julia-mode-hook 'ws-butler-mode)
 
-(add-hook 'julia-repl-hook #'julia-repl-use-emacsclient)
+(use-package vterm)
+(use-package julia-snail
+  :hook (julia-mode . julia-snail-mode))
