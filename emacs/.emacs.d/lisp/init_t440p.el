90fdc78d94a0656b6768afb12f3aac920225b69a
Robert Schwarz <mail@rschwarz.net>
2021-04-07 11:33:25 +0200

    emacs: don't use chrome

--- emacs/.emacs.d/lisp/init_t440p.el
+++ emacs/.emacs.d/lisp/init_t440p.el
@@ -1,5 +1,2 @@
 ;; customizations to be used only on this machine
 
-;; open links from org in chromium, not firefox
-(setq browse-url-browser-function 'browse-url-generic
-      browse-url-generic-program "google-chrome")
