aa751e3949115fb616095db8565011929c0b6388
Robert Schwarz <mail@rschwarz.net>
2022-02-04 09:56:48 +0100

    emacs: use elpy for python

--- emacs/.emacs.d/lisp/init_python.el
+++ emacs/.emacs.d/lisp/init_python.el
@@ -1,25 +1,13 @@
 ;; python specific customizations
 
-;; python-jedi
-;; (setq jedi:server-command (quote ("python2" ".../jediepcserver.py")))
-;; (setq jedi:setup-keys t)
-;; (autoload 'jedi:setup "jedi" nil t)
-;; (add-hook 'python-mode-hook 'jedi:setup)
-
-;; (defun autopep8 ()
-;;   (interactive)
-;;   (shell-command-on-region
-;;    (point-min)
-;;    (point-max)
-;;    "autopep8 -"
-;;    t))
-
-(use-package sphinx-doc)
+(use-package elpy :ensure t :init (elpy-enable))
 
 (add-hook 'python-mode-hook   (lambda () (sphinx-doc-mode 1)))
 (add-hook 'python-mode-hook   (lambda () (subword-mode 1)))
 (add-hook 'python-mode-hook   (lambda () (rainbow-delimiters-mode 1)))
 (add-hook 'python-mode-hook   (lambda () (idle-highlight-mode 1)))
 
+(use-package sphinx-doc)
+
 (use-package cython-mode :ensure nil)
-(add-to-list 'auto-mode-alist '("\\.sage\\'" . cython-mode))
+
