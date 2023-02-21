9d6f642eef37511538ea701008fb60e44d75dda1
Robert Schwarz <mail@rschwarz.net>
2023-02-21 10:27:27 +0100

    emacs: disable elpy until fixed

--- emacs/.emacs.d/lisp/init_python.el
+++ emacs/.emacs.d/lisp/init_python.el
@@ -1,6 +1,6 @@
 ;; python specific customizations
 
-(use-package elpy :ensure t :init (elpy-enable))
+;(use-package elpy :ensure t :init (elpy-enable))
 
 (add-hook 'python-mode-hook   (lambda () (sphinx-doc-mode 1)))
 (add-hook 'python-mode-hook   (lambda () (subword-mode 1)))
