693f929ae39758c491e21d9731c7994006dce539
Robert Schwarz <robert@doingthemath.nl>
2023-04-11 08:36:59 +0200

    emacs: add missing package archive (for org-contrib)

--- emacs/.emacs.d/lisp/init_package.el
+++ emacs/.emacs.d/lisp/init_package.el
@@ -3,6 +3,7 @@
 ;; add repos
 ;; (package-initialize) ; no longer necessary?!
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
 
 ;; use-package
 (require 'use-package)
