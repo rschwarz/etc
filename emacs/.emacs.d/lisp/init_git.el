c4965fd9f070a6a6e4445f23d59501279bec224b
Robert Schwarz <robert@doingthemath.nl>
2026-02-04 14:04:59 +0100

    emasc: add magit-pre-commit

--- emacs/.emacs.d/lisp/init_git.el
+++ emacs/.emacs.d/lisp/init_git.el
@@ -5,3 +5,5 @@
 (setq-default magit-default-tracking-name-function
               (quote magit-default-tracking-name-branch-only))
 (setq-default magit-push-always-verify nil)
+
+(use-package magit-pre-commit :after magit)
